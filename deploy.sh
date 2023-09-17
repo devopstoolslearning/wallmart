10405158e8c64c4354a818dd07e4b22a49a91c78
pattanimran57 <pattanimrankhan57@gmail.com>
2023-09-17 18:16:31 +0530

    complete

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,11 @@
-hello
-we are deploying the code
+class factorial{  
+public static void main(String args[]){  
+int i, fact=1;  
+int n=5;  
+for(i=1;i<=5;i++)  
+{  
+fact=fact*i;  
+}  
+System.out.println("Factorial of "+ n+ " is: "+fact);  
+}  
+}  
