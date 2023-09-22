3b976252ac74be45d62af4c4c4a27781e6f7c83b
pattanimran57 <pattanimran57@gmail.com>
2023-09-22 13:23:14 +0530

    commit

---
+++ Book.py
@@ -0,0 +1,9 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def display_info(self):
+        print(f"Title: {self.title}")
+        print(f"Author: {self.author}")
+
